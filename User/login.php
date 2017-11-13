bb966c947a273289f08b6802767e1116a9537fa4
JeanSolo10 <jeanpksb10@gmail.com>
2017-11-13 00:17:15 -0500

    User Log In
    PHP file for users to get access to site via Facebook authentication

---
+++ User/login.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content= width=device- width initial-scale=1.0 maximum-scale=1.0 user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge",chrome=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <title>Log In</title>
+</head>
+
+
+<body>
+
+    <div class="container" style="margin-top: 100px">
+        <div class="col-md-6 col-md-offset-3" align="center">
+            <img src=""><br>
+            <form>
+                <input  name="email" placeholder="Email" class="form-control"><br>
+                <input  name="email" type="password" placeholder="Password" class="form-control"><br>
+                <input type="submit" value="Log In" class="btn btn-primary">
+                <input type="button" value="Log In with Facebook" class="btn btn-primary">
+
+            </form>
+
+        </div>
+    </div>
+
+
+</body>
+</html>
